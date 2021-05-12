f2fbb4795ad949a4109ec3959cd24072cce1d014
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-05-12 10:11:29 +0200

    add js

--- script.js
+++ script.js
@@ -2,8 +2,32 @@ new Vue({
     el: "#app",
 
     data: {
-        userEmail: null,
-        randomEmail: [],
+        mailList: [],
+        mails: [],
+        loading: false
+    },
+
+    methods: {
+        getRandomEmails() {
+            this.loading = true;
+            this.mailList = [];
+
+            for (let i = 0; i < 10; i++) {
+                setTimeout(() => {
+                    axios.get("https://flynn.boolean.careers/exercises/api/random/int")
+                        .then((resp) => {
+                            let randomMail = resp.data.response;
+                            this.mails.push(randomMail);
+
+                            if (this.mail.length == 10){
+                                this.mailList = [...this.mails]
+                                this.loading = false
+                            }
+
+                        })
+                },Math.floor(math.random()*1000))
+            }
+        }
     }
 
-})    
+})
